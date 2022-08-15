4f9c6becdb500ff3b01763a934082458dc26a077
LionelMv <mvangicode@gmail.com>
2022-08-15 20:37:35 +0300

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,12 +3,12 @@
 /**
  * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
- *
+ * @count: counts the number of elements
  * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num = 0;
+	size_t count = 0;
 
 /**
  * Not creating another ptr is because,
@@ -18,9 +18,9 @@ size_t print_listint(const listint_t *h)
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
-		num++;
+		count++;
 		h = h->next;
 	}
 
-	return (num);
+	return (count);
 }

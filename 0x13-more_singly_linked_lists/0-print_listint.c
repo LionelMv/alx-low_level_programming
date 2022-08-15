8e439d38e1e799e87056d0c61373954386111b05
LionelMv <mvangicode@gmail.com>
2022-08-15 20:32:29 +0300

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a linked list
+ * @h: linked list of type listint_t to print
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t num = 0;
+
+/** Not creating another ptr is because,
+ * we are not dealing with the head of the node,
+ * but the transversing pointer in main.
+ */
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		num++;
+		h = h->next;
+	}
+
+	return (num);
+}
